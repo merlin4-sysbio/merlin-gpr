91866aaf8b8b75b3ab2633322391870518fb6304
Oscar Dias <odias@ceb.uminho.pt>
2016-10-28 18:11:48 +0100

    updates to methods

--- src/main/java/pt/uminho/sysbio/merlin/gpr/rules/core/IdentifyGenomeSubunits.java
+++ src/main/java/pt/uminho/sysbio/merlin/gpr/rules/core/IdentifyGenomeSubunits.java
@@ -16,7 +16,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.biojava.nbio.alignment.Alignments.PairwiseSequenceScorerType;
 import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +28,7 @@ import pt.uminho.sysbio.common.database.connector.databaseAPI.containers.gpr.Rea
 import pt.uminho.sysbio.common.database.connector.databaseAPI.containers.gpr.ReactionsGPR_CI;
 import pt.uminho.sysbio.common.database.connector.datatypes.Connection;
 import pt.uminho.sysbio.common.database.connector.datatypes.DatabaseAccess;
+import pt.uminho.sysbio.common.local.alignments.core.Enumerators.AlignmentScoreType;
 import pt.uminho.sysbio.common.local.alignments.core.Enumerators.Method;
 import pt.uminho.sysbio.common.local.alignments.core.PairwiseSequenceAlignement;
 import pt.uminho.sysbio.common.local.alignments.core.RunSimilaritySearch;
@@ -177,7 +177,7 @@ public class IdentifyGenomeSubunits extends Observable implements Observer {
 						if(orthologs.size()>0 && !this.cancel.get()) {
 
 							RunSimilaritySearch search = new RunSimilaritySearch(this.dba, this.genome, this.similarity_threshold, 
-									this.method, orthologs, this.cancel, new AtomicInteger(0), new AtomicInteger(0), -1, PairwiseSequenceScorerType.LOCAL_IDENTITIES);
+									this.method, orthologs, this.cancel, new AtomicInteger(0), new AtomicInteger(0), -1, AlignmentScoreType.IDENTITY);
 
 							//search.addObserver(this);
 							search.setEc_number(ec_number);
